cf006c62617d66c7d2288ab577326f8d251d7108
Stuart McPherson <stuart.mcpherson@flipmind.com>
2020-11-13 14:01:06 +1300

    api change

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,15 @@ export default {
 </script>
 
 <style>
+
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
 html {
   font-family: "Open Sans", sans-serif;
   font-size: 18px;
--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-const siteURL = "https://css-tricks.com"
+const siteURL = "https://flipmind.com"
 
 export const state = () => ({
   posts: [],
